e6bf11cc57da82bd590cd04390b78c0385a0515e
JRB <jrb@abv.bg>
2023-01-22 21:06:24 +0200

    Add Queue, DoublyLinkedList and ArrayDeque logic

--- 03-Exercise-Data Linear Structures/src/main/java/implementations/ArrayDeque.java
+++ 03-Exercise-Data Linear Structures/src/main/java/implementations/ArrayDeque.java
@@ -5,6 +5,16 @@ import interfaces.Deque;
 import java.util.Iterator;
 
 public class ArrayDeque<E> implements Deque<E> {
+    private final int INITIAL_CAPACITY = 7;
+
+    private int size;
+
+    private Object[] elements;
+
+    public ArrayDeque() {
+        this.elements =new Object[INITIAL_CAPACITY];
+    }
+
     @Override
     public void add(E Element) {
 
