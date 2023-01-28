a4cb1d56fbc8bf97aaa0e9a08becad30e0efa3af
JRB <jrb@abv.bg>
2023-01-29 00:58:25 +0200

    Add implementation Hierarchy and 2-3 Tree

--- 11-Exercise-B-Trees and AVL Trees/src/main/java/Main.java
+++ 11-Exercise-B-Trees and AVL Trees/src/main/java/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
+        TwoThreeTree<Integer> integerTwoThreeTree = new TwoThreeTree<>();
 
+        integerTwoThreeTree.insert(15);
+        integerTwoThreeTree.insert(10);
+        integerTwoThreeTree.insert(20);
     }
 }
--- 11-Exercise-B-Trees and AVL Trees/src/main/java/TwoThreeTree.java
+++ 11-Exercise-B-Trees and AVL Trees/src/main/java/TwoThreeTree.java
@@ -1,7 +1,7 @@
 public class TwoThreeTree<K extends Comparable<K>> {
     private TreeNode<K> root;
 
-    public static class TreeNode<K> {
+    private static class TreeNode<K> {
         private K leftKey;
         private K rightKey;
 
@@ -11,6 +11,12 @@ public class TwoThreeTree<K extends Comparable<K>> {
 
         private TreeNode(K key) {
             this.leftKey = key;
+        }
+
+        private TreeNode(K left, K middle, K right) {
+            this.leftKey = middle;
+            this.leftChild = new TreeNode<K>(left);
+            this.rightChild = new TreeNode<K>(right);
         }
 
         boolean isThreeNode() {
@@ -26,8 +32,107 @@ public class TwoThreeTree<K extends Comparable<K>> {
         }
     }
 
-    public void insert(K key) {
+    private TreeNode<K> internalInsert(TreeNode<K> currentNode, K value) {
+        if (currentNode.isLeaf()) {
+            if (currentNode.isTwoNode()) {
+                if (value.compareTo(currentNode.leftKey) < 0) {
+                    currentNode.rightKey = currentNode.leftKey;
+                    currentNode.leftKey = value;
+                } else {
+                    currentNode.rightKey = value;
+                }
+            } else {
+                K left = currentNode.leftKey; // Left key
+                K middle = value; // Value to add
+                K right = currentNode.rightKey; // Right key
 
+                if (value.compareTo(left) < 0) {
+                    return new TreeNode<>(middle, left, right);
+                } else if (value.compareTo(right) > 0) {
+                    return new TreeNode<>(left, right, middle);
+                } else {
+                    return new TreeNode<>(left, middle, right);
+                }
+            }
+        } else {
+            TreeNode<K> restructuredNode = null;
+
+            if (currentNode.isTwoNode() && value.compareTo(currentNode.leftKey) < 0) {
+                restructuredNode = this.internalInsert(currentNode.leftChild, value);
+            } else if (currentNode.isTwoNode() && value.compareTo(currentNode.leftKey) > 0) {
+                restructuredNode = this.internalInsert(currentNode.rightChild, value);
+            } else if (currentNode.isThreeNode() && value.compareTo(currentNode.leftKey) < 0) {
+                restructuredNode = this.internalInsert(currentNode.leftChild, value);
+            } else if (currentNode.isThreeNode() && value.compareTo(currentNode.rightKey) > 0) {
+                restructuredNode = this.internalInsert(currentNode.rightChild, value);
+            } else {
+                restructuredNode = this.internalInsert(currentNode.middleChild, value);
+            }
+
+            if (restructuredNode != null) {
+                if (currentNode.isTwoNode()) {
+                    if (restructuredNode.leftKey.compareTo(currentNode.leftKey) < 0) {
+                        currentNode.rightKey = currentNode.leftKey;
+                        currentNode.leftKey = restructuredNode.leftKey;
+
+                        currentNode.leftChild = restructuredNode.leftChild;
+                        currentNode.middleChild = restructuredNode.rightChild;
+                    } else {
+                        currentNode.rightKey = restructuredNode.leftKey;
+
+                        currentNode.middleChild = restructuredNode.leftChild;
+                        currentNode.rightChild = restructuredNode.rightChild;
+                    }
+                } else {
+                    TreeNode<K> newNode = null;
+
+                    if(restructuredNode.leftKey.compareTo(currentNode.leftKey) < 0) {
+                        newNode = new TreeNode<>(currentNode.leftKey);
+
+                        newNode.leftChild = restructuredNode;
+                        newNode.rightChild = new TreeNode<K>(currentNode.rightKey);
+
+                        newNode.rightChild.leftChild = currentNode.middleChild;
+                        newNode.rightChild.rightChild = currentNode.rightChild;
+                    } else if (restructuredNode.leftKey.compareTo(currentNode.rightKey) > 0) {
+                        newNode = new TreeNode<>(currentNode.rightKey);
+
+                        newNode.leftChild = new TreeNode<K>(currentNode.leftKey);
+                        newNode.rightChild = restructuredNode;
+
+                        newNode.leftChild.leftChild = currentNode.leftChild;
+                        newNode.leftChild.rightChild = currentNode.middleChild;
+                    } else {
+                        newNode = new TreeNode<>(restructuredNode.leftKey);
+
+                        newNode.leftChild = new TreeNode<K>(currentNode.leftKey);
+                        newNode.rightChild = new TreeNode<K>(currentNode.rightKey);
+
+                        newNode.leftChild.leftChild = currentNode.leftChild;
+                        newNode.leftChild.rightChild = restructuredNode.leftChild;
+
+                        newNode.rightChild.rightChild = currentNode.rightChild;
+                        newNode.rightChild.leftChild = restructuredNode.rightChild;
+                    }
+
+                    return newNode;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public void insert(K key) {
+        if (this.root == null) {
+            this.root = new TreeNode<K>(key);
+        } else {
+            TreeNode<K> node = this.internalInsert(this.root, key);
+
+            if (node != null) {
+                this.root = node;
+            }
+        }
     }
 
     public String getAsString() {
