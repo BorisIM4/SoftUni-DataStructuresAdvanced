4a3d9b3234d1445671660a67d5bbd06226d4bf0b
JRB <jrb@abv.bg>
2023-02-01 19:42:00 +0200

    add tests

--- 11-Exercise-B-Trees and AVL Trees/src/main/java/Main.java
+++ 11-Exercise-B-Trees and AVL Trees/src/main/java/Main.java
@@ -3,8 +3,10 @@ public class Main {
     public static void main(String[] args) {
         TwoThreeTree<Integer> integerTwoThreeTree = new TwoThreeTree<>();
 
-        integerTwoThreeTree.insert(15);
         integerTwoThreeTree.insert(10);
+        integerTwoThreeTree.insert(15);
         integerTwoThreeTree.insert(20);
+        integerTwoThreeTree.insert(30);
+        integerTwoThreeTree.insert(40);
     }
 }
